9e251e9f5e6c2fdbc5b8edfb598a59fa004264b4
w-artisan <mailtome@i.ua>
2020-12-11 13:58:01 +0200

    Add: sentence_smash.rb

---
+++ 04_Codewars_(8kyu)/sentence_smash.rb
@@ -0,0 +1,22 @@
+# Sentence Smash
+# https://www.codewars.com/kata/53dc23c68a0c93699800041d/ruby
+
+# Sentence Smash
+
+# Write a function that takes an array of words and smashes
+# them together into a sentence and returns the sentence.
+# You can ignore any need to sanitize words or add punctuation,
+# but you should add spaces between each word.
+# Be careful, there shouldn't be a space at
+# the beginning or the end of the sentence!
+# Example
+
+# ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+def smash(words)
+  words.join(' ')
+end
+
+# Sample Tests:
+# Test.assert_equals(smash(["hello"]), "hello")
+# Test.assert_equals(smash(["hello", "world"]), "hello world")
