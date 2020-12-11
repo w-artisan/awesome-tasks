9fb8d11aeb513671d6cf21c93e3f25dd0aef9925
w-artisan <mailtome@i.ua>
2020-12-11 13:41:23 +0200

    Add: you_cant_code_under_pressure_1.rb

---
+++ 04_Codewars_(8kyu)/you_cant_code_under_pressure_1.rb
@@ -0,0 +1,11 @@
+# You Can't Code Under Pressure #1
+# https://www.codewars.com/kata/53ee5429ba190077850011d4/ruby
+
+# Code as fast as you can! You need to double the integer and return it.
+
+def double_integer(i)
+  i * 2
+end
+
+# Sample Tests:
+# Test.assert_equals(double_integer(6), 12)
