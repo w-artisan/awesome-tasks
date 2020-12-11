34808851da947ffd4e1a8903830c190c8137695b
w-artisan <mailtome@i.ua>
2020-12-11 12:48:37 +0200

    Add: 1_hello_world.rb

---
+++ 02_Tasks-Part2-(Main)/1_hello_world.rb
@@ -0,0 +1,8 @@
+# === Task 1 ===
+# Створити змінні text1='Hello' і text2='World'.
+# За допомогою цих змінних і операції складання рядків
+# вивести на екран фразу «Hello world».
+
+text1 = 'Hello'
+text2 = 'World'
+puts "#{text1} #{text2}"
