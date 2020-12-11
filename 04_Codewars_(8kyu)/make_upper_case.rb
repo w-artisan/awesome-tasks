015d063091e264515085c75b9e1827fa92a963ab
w-artisan <mailtome@i.ua>
2020-12-11 13:55:39 +0200

    Add: make_upper_case.rb

---
+++ 04_Codewars_(8kyu)/make_upper_case.rb
@@ -0,0 +1,15 @@
+# MakeUpperCase
+# https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/ruby
+
+# Write a function which converts the input string to uppercase.
+
+def make_upper_case(str)
+  str.upcase
+end
+
+# Sample Tests:
+# describe "Basic Tests" do
+#   it "should pass the basic tests" do
+#     Test.assert_equals(make_upper_case("hello"), "HELLO")
+#   end
+# end
