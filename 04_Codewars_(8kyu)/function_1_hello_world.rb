0448d2cfade4a5be11651a312ac04eb9024cb0a9
w-artisan <mailtome@i.ua>
2020-12-11 13:23:16 +0200

    Add: function_1_hello_world.rb

---
+++ 04_Codewars_(8kyu)/function_1_hello_world.rb
@@ -0,0 +1,22 @@
+# Function 1 - hello world
+# https://www.codewars.com/kata/523b4ff7adca849afe000035/ruby
+
+# Description:
+
+# Make a simple function called greet that returns the most-famous "hello world!".
+# Style Points
+
+# Sure, this is about as easy as it gets. But how clever can you be to create
+# the most creative hello world you can think of? What is a "hello world"
+# solution you would want to show your friends?
+
+def greet
+  'hello world!'
+end
+
+# Sample Tests:
+# describe "Testing function" do
+#   it "Correct return-value?" do
+#     Test.assert_equals(greet, "hello world!")
+#   end
+# end
