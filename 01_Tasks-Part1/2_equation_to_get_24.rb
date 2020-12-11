422b5ccdde8bffbf4e003d3bcf4f58aac09412ec
w-artisan <mailtome@i.ua>
2020-12-11 12:41:38 +0200

    Add: 2_equation_to_get_24.rb

---
+++ 01_Tasks-Part1/2_equation_to_get_24.rb
@@ -0,0 +1,6 @@
+# 2. What is the equation to get 24 with 1,3,4 and 6
+# you can only divide, multiply or add them?
+# Output example: 24 == 1 / 3 * 4 + 6
+
+## === === Solution === ===
+p 6 * 4 + 1 / 3 #=> 24
