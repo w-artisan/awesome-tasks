8be465b2d159d3c58e01a7fac62996f5ebb58972
w-artisan <mailtome@i.ua>
2020-12-11 14:03:40 +0200

    Add: function_3_multiplying_two_numbers.rb

---
+++ 04_Codewars_(8kyu)/function_3_multiplying_two_numbers.rb
@@ -0,0 +1,11 @@
+# Function 3 - multiplying two numbers
+# https://www.codewars.com/kata/523b66342d0c301ae400003b/ruby
+
+# Implement a function which multiplies two numbers.
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+# Sample Tests:
+# Test.assert_equals(multiply(2, 3), 6)
