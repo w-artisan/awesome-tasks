32f4061263bb2d5411d95c7e60cc10d7f8b41dc7
w-artisan <mailtome@i.ua>
2020-12-11 13:54:01 +0200

    Add: convert_a_boolean_to_a_string.rb

---
+++ 04_Codewars_(8kyu)/convert_a_boolean_to_a_string.rb
@@ -0,0 +1,13 @@
+# Convert a Boolean to a String
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd/ruby
+
+# Implement a function which convert the given
+# boolean value into its string representation.
+
+def boolean_to_string(bool)
+  bool.to_s
+end
+
+# Sample Tests:
+# Test.assert_equals(boolean_to_string(true), "true")
+# Test.assert_equals(boolean_to_string(false), "false")
