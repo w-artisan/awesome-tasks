015ac68ce85cb40786cc8089cf417cb2b0a62f02
w-artisan <mailtome@i.ua>
2020-12-17 16:44:47 +0200

    Refactor: Add 'bool_to_equality' method

--- 03_Bonus-Tasks/1_compare_sum_char_code.rb
+++ 03_Bonus-Tasks/1_compare_sum_char_code.rb
@@ -16,17 +16,21 @@
 # null, "" -> equal
 # Your method should return true, if the strings are equal and false if they are not equal.
 
-null = ''
 def compare_sum_char_code(chars1, chars2)
   num1 = chars1.to_s.upcase.split('').map(&:ord).sum
   num2 = chars2.to_s.upcase.split('').map(&:ord).sum
-  num2.zero? ? (p true) : (p num1 == num2)
+  num2.zero? ? true : num1 == num2
 end
 
-compare_sum_char_code('AD', 'BC') #=> true
-compare_sum_char_code('AD', 'DD') #=> false
-compare_sum_char_code('gf', 'FG') #=> true
-compare_sum_char_code('zz1', '')  #=> true
-compare_sum_char_code('ZzZz', 'ffPFF') #=> true
-compare_sum_char_code('kl', 'lz') #=> false
-compare_sum_char_code(null, '')   #=> true # null = ''
+def bool_to_equality(bool)
+  bool ? 'equal' : 'not equal'
+end
+
+null = nil
+puts bool_to_equality(compare_sum_char_code('AD', 'BC')) #=> equal
+puts bool_to_equality(compare_sum_char_code('AD', 'DD')) #=> not equal
+puts bool_to_equality(compare_sum_char_code('gf', 'FG')) #=> equal
+puts bool_to_equality(compare_sum_char_code('zz1', ''))  #=> equal
+puts bool_to_equality(compare_sum_char_code('ZzZz', 'ffPFF')) #=> equal
+puts bool_to_equality(compare_sum_char_code('kl', 'lz')) #=> not equal
+puts bool_to_equality(compare_sum_char_code(null, ''))   #=> equal
